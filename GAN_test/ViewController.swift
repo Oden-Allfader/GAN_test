7c210ad0161ca98989c9671b5279fef6ec1d8dad
Jonatan Atles <typdude@hotmail.com>
2019-02-27 09:18:31 +0100

    Initial Commit

---
+++ GAN_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GAN_test
+//
+//  Created by Jonatan on 2019-02-27.
+//  Copyright © 2019 Mr. Jay Jayway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
